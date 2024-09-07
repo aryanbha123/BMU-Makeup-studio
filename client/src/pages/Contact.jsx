2b8237ec31d201b566ca34a832e8537cb2416ae7
Aaryan Bhandari <aryanbhandari4077@gmail.com>
2024-09-07 14:53:09 +0530

    push

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -3,7 +3,7 @@ export default function Contact() {
     return (
         <div>
  
-            <div class="lg:px-20 px-5   bg-white my-6 font-[sans-serif]">
+            <div class="lg:px-20 px-5   mt-6 mb-20 font-[sans-serif]">
                 {/* <div class="text-center px-6">
                     <h2 class="text-gray-800 text-3xl font-extrabold">Contact Us</h2>
                     <p class="text-sm text-gray-500 mt-4">Have some big idea or brand to develop and need help?</p>
@@ -84,7 +84,7 @@ export default function Contact() {
                             <div class="grid sm:grid-cols-2 gap-8">
                                 <div class="relative flex items-center">
                                     <input type="text" placeholder="First Name"
-                                        class="px-2 py-3 bg-white w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-500 outline-none" />
+                                        class="px-2 py-3 bg-transparent w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-500 outline-none" />
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2"
                                         viewBox="0 0 24 24">
                                         <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
@@ -96,7 +96,7 @@ export default function Contact() {
 
                                 <div class="relative flex items-center">
                                     <input type="text" placeholder="Last Name"
-                                        class="px-2 py-3 bg-white w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-500 outline-none" />
+                                        class="px-2 py-3 bg-transparent w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-500 outline-none" />
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2"
                                         viewBox="0 0 24 24">
                                         <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
@@ -108,7 +108,7 @@ export default function Contact() {
 
                                 <div class="relative flex items-center">
                                     <input type="number" placeholder="Phone No."
-                                        class="px-2 py-3 bg-white text-black w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-500 outline-none" />
+                                        class="px-2 py-3 bg-transparent text-black w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-500 outline-none" />
                                     <svg fill="#bbb" class="w-[18px] h-[18px] absolute right-2" viewBox="0 0 64 64">
                                         <path
                                             d="m52.148 42.678-6.479-4.527a5 5 0 0 0-6.963 1.238l-1.504 2.156c-2.52-1.69-5.333-4.05-8.014-6.732-2.68-2.68-5.04-5.493-6.73-8.013l2.154-1.504a4.96 4.96 0 0 0 2.064-3.225 4.98 4.98 0 0 0-.826-3.739l-4.525-6.478C20.378 10.5 18.85 9.69 17.24 9.69a4.69 4.69 0 0 0-1.628.291 8.97 8.97 0 0 0-1.685.828l-.895.63a6.782 6.782 0 0 0-.63.563c-1.092 1.09-1.866 2.472-2.303 4.104-1.865 6.99 2.754 17.561 11.495 26.301 7.34 7.34 16.157 11.9 23.011 11.9 1.175 0 2.281-.136 3.29-.406 1.633-.436 3.014-1.21 4.105-2.302.199-.199.388-.407.591-.67l.63-.899a9.007 9.007 0 0 0 .798-1.64c.763-2.06-.007-4.41-1.871-5.713z"
@@ -118,7 +118,7 @@ export default function Contact() {
 
                                 <div class="relative flex items-center">
                                     <input type="email" placeholder="Email"
-                                        class="px-2 py-3 bg-white text-black w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-500 outline-none" />
+                                        class="px-2 py-3 bg-transparent text-black w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-500 outline-none" />
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2"
                                         viewBox="0 0 682.667 682.667">
                                         <defs>
@@ -139,7 +139,7 @@ export default function Contact() {
 
                                 <div class="relative flex items-center sm:col-span-2">
                                     <textarea placeholder="Write Message"
-                                        class="px-2 pt-3 bg-white text-black w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-500 outline-none"></textarea>
+                                        class="px-2 pt-3 bg-transparent text-black w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-500 outline-none"></textarea>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2"
                                         viewBox="0 0 682.667 682.667">
                                         <defs>
