ef6160f4afff905f7f6c1f50459ead1e90fc03bc
Pranjal-Rawat <pranjalr842@gmail.com>
2024-09-07 14:43:05 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,10 @@ import Slider from '../components/Slider';
 import Service from '../components/Service';
 import EnterpriseSection from '../components/EnterPriseSection';
 import Map from '../components/Map';
+import bmu1 from '../assets/IMG-20240907-WA0003.jpg';
+import bmu2 from '../assets/IMG-20240907-WA0004.jpg';
+import bmu3 from '../assets/IMG-20240907-WA0005.jpg';
+import bmu4 from '../assets/IMG-20240907-WA0006.jpg';
 
 export default function Home() {
 
@@ -52,7 +56,7 @@ export default function Home() {
               variants={scaleUp}
             >
               <a href="#" className="group relative flex flex-col overflow-hidden rounded-lg pb-4 pt-40 flex-grow">
-                <img src="https://images.unsplash.com/photo-1510812431401-41d2bd2722f3?q=80&w=2940&auto=format&fit=crop" alt="Makeup" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
+                <img src={bmu1} alt="Makeup" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
                 <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
                 <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Skincare</h3>
               </a>
@@ -66,18 +70,18 @@ export default function Home() {
               variants={scaleUp}
             >
               <a href="#" className="group relative flex flex-col overflow-hidden rounded-lg pb-4 pt-40 mb-4">
-                <img src="https://images.unsplash.com/photo-1504675099198-7023dd85f5a3?q=80&w=2940&auto=format&fit=crop" alt="Makeup Tools" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
+                <img src={bmu2} alt="Makeup Tools" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
                 <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
                 <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Makeup Tools</h3>
               </a>
               <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-2">
                 <a href="#" className="group relative flex flex-col overflow-hidden rounded-lg pb-4 pt-40">
-                  <img src="https://images.unsplash.com/photo-1571104508999-893933ded431?q=80&w=2940&auto=format&fit=crop" alt="Nail Care" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
+                  <img src={bmu3} alt="Nail Care" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
                   <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
                   <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Nail Care</h3>
                 </a>
                 <a href="#" className="group relative flex flex-col overflow-hidden rounded-lg pb-4 pt-40">
-                  <img src="https://images.unsplash.com/photo-1626897505254-e0f811aa9bf7?q=80&w=2940&auto=format&fit=crop" alt="Fragrances" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
+                  <img src={bmu4} alt="Fragrances" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
                   <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
                   <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Fragrances</h3>
                 </a>
