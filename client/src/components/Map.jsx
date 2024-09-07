f54855a63af660b4de8f808464dee1205dd2397b
Aaryan Bhandari <aryanbhandari4077@gmail.com>
2024-09-07 15:33:23 +0530

    push

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -4,7 +4,10 @@ export default function Map() {
     return (
         <div>
             <div className="lg:px-12 px-5 mb-4">
-                <div className='max-w-[100%] w-[100vw] h-[60vh] '><div id="my-map-display" style={{ height: "100%", width: "100%", maxWidth: "100%" }}><iframe style={{ height: "100%", width: "100%", border: "0", frameborder: "0" }} src="https://www.google.com/maps/embed/v1/place?q=BMU+Makeup+studio+Dehradun&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="my-codefor-googlemap" href="https://www.bootstrapskins.com/themes" id="grab-map-authorization">Visit Us</a>
+                <div className='max-w-[100%] w-[100vw] h-[60vh] '><div id="my-map-display" style={{ height: "100%", width: "100%", maxWidth: "100%" }}><iframe style={{ height: "100%", width: "100%", border: "0", frameborder: "0" }} src="https://www.google.com/maps/embed/v1/place?q=BMU+Makeup+studio+Dehradun&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div>
+                </div>
+                <div className='flex justify-center'>
+                    <a href="" className="bg-black mt-4 text-center rounded-[18px] text-white px-5 py-2 ">Visit Us</a>
                 </div>
             </div>
         </div>
